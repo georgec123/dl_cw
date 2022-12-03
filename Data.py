b2868b524309561c1b66611d1d74671a4a999696
JMCG22 <118627389+JMCG22@users.noreply.github.com>
2022-12-03 10:52:21 +0000

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+
+def organise_data(split):
+    df = pd.read_csv('DL-2022-CW-data (1)/Data_A.csv', header=None)
+    df.columns = ['label', 's_1_p', 's_1_v', 'b_1_p', 'b_1_v', 's_2_p', 's_2_v', 'b_2_p', 'b_2_v', 's_3_p', 's_3_v',
+                  'b_3_p', 'b_3_v', 's_4_p', 's_4_v', 'b_4_p', 'b_4_v', 'mid_1', 'mid_2', 'mid_3', 'mid_4', 'mid_5']
+    train = df[:split]
+    test = df[split:].reset_index()
+    test = test.drop(['index'], axis=1)
+
+    numeric_names = ['s_1_p', 's_1_v', 'b_1_p', 'b_1_v', 's_2_p', 's_2_v', 'b_2_p', 'b_2_v', 's_3_p', 's_3_v',
+                  'b_3_p', 'b_3_v', 's_4_p', 's_4_v', 'b_4_p', 'b_4_v']
+    binary_names = ['mid_1', 'mid_2', 'mid_3', 'mid_4', 'mid_5']
+    
+    return [train, test, numeric_names, binary_names]
+
+#arr = organise_data(90000)
+#print(arr[1].iloc[:, 1:23].to_numpy())
+#print(arr[0]['label'])
+#print(arr[1])
+
+def accuracy(list1, list2):
+    total = 0
+    L = len(list1)
+
+    for i in range(L):
+        if list1[i] == list2[i]:
+            total += 1
+    
+    return total/L
---
+++ FNN.py
@@ -0,0 +1,107 @@
+import tensorflow.keras as keras
+import tensorflow as tf
+import numpy as np
+import pandas as pf
+import matplotlib.pyplot as plt
+from Data import organise_data, accuracy
+
+arr = organise_data(80000)
+train = arr[0]
+test = arr[1]
+numeric_names = arr[2]
+binary_names = arr[3]
+
+n = 100
+
+def sigmoid(x):
+    return 1 / (1+np.exp(-x))
+
+def stack_dict(inputs, fun=tf.stack):
+    values = []
+    for key in sorted(inputs.keys()):
+      values.append(tf.cast(inputs[key], tf.float32))
+
+    return fun(values, axis=-1)
+
+def preprocess(df, numeric_names, binary_names):
+    target = df.pop('label')
+    numeric_features = df[numeric_names]
+    binary_features = df[binary_names]
+    tf.convert_to_tensor(numeric_features)
+
+    inputs = {}
+    for name, column in df.items():
+        if type(column[0]) == str:
+            dtype = tf.string
+        elif (name in binary_names):
+            dtype = tf.int64
+        else:
+            dtype = tf.float32
+
+        inputs[name] = tf.keras.Input(shape=(), name=name, dtype=dtype)
+
+    preprocessed = []
+
+    for name in binary_names:
+        inp = inputs[name]
+        inp = inp[:, tf.newaxis]
+        float_value = tf.cast(inp, tf.float32)
+        preprocessed.append(float_value)
+    
+    preprocessed
+
+    normalizer = tf.keras.layers.Normalization(axis=-1)
+    normalizer.adapt(stack_dict(dict(numeric_features))) 
+    numeric_inputs = {}
+    for name in numeric_names:
+        numeric_inputs[name]=inputs[name]
+
+    numeric_inputs = stack_dict(numeric_inputs)
+    numeric_normalized = normalizer(numeric_inputs)
+
+    preprocessed.append(numeric_normalized)
+
+    preprocesssed_result = tf.concat(preprocessed, axis=-1)
+    preprocesssed_result
+    preprocessor = tf.keras.Model(inputs, preprocesssed_result)
+    x = preprocessor(inputs)
+
+    return [x, inputs, target]
+
+def fit_FNN(df, numeric_names, binary_names):
+    pre = preprocess(df, numeric_names, binary_names)
+    x = pre[0]
+    inputs = pre[1]
+    target = pre[2]
+
+    f = keras.Sequential([
+        keras.layers.InputLayer(input_shape=(21,)),
+        keras.layers.Dense(n, activation="elu"),
+        keras.layers.Dense(1, activation="sigmoid")
+    ])
+
+    res = f(x)
+
+    model = tf.keras.Model(inputs, res)
+
+    model.compile(optimizer='sgd',
+                loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+    callbacks_used = [
+        tf.keras.callbacks.ReduceLROnPlateau(monitor='accuracy',factor=0.2,patience=5,min_lr=0)
+    ]
+    
+    model.fit(dict(df), target, epochs=5, batch_size=100, callbacks=callbacks_used)
+
+    return model
+
+def predict(f, df, numeric_names, binary_names):
+
+    Y_hat = (f.predict(dict(df)) > 0.5).astype("int32")
+
+    return Y_hat
+
+h = fit_FNN(train, numeric_names, binary_names)
+Y_hat = predict(h, test, numeric_names, binary_names)
+print(Y_hat)
+print(accuracy(Y_hat, test['label'].tolist()))
